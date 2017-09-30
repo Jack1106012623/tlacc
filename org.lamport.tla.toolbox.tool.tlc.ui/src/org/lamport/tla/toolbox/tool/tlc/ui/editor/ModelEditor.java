Jack1106012623/tlacc

commit 141b40c45289cdbeae871af440595a13a6026592

Do not propagate model changes through Eclipse file markers. Listening for file markers results in an event storm when a model is launched.
[Refactor][Toolbox]

org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/ui/editor/ModelEditor.java
@@ -5,7 +5,6 @@ import java.text.SimpleDateFormat;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IMarker;
-import org.eclipse.core.resources.IMarkerDelta;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
@@ -86,8 +85,22 @@ public class ModelEditor extends FormEditor
      */
     // helper to resolve semantic matches of words
     private SemanticHelper helper;
-    // reacts on model changes
-    private IResourceChangeListener modelFileChangeListener;
+    private final Model.StateChangeListener modelStateListener = new Model.StateChangeListener() {
+		@Override
+		public void handleChange(ChangeEvent event) {
+			UIHelper.runUIAsync(new Runnable() {
+				public void run() {
+					for (int i = 0; i < getPageCount(); i++) {
+						final Object object = pages.get(i);
+						if (object instanceof BasicFormPage) {
+							final BasicFormPage bfp = (BasicFormPage) object;
+							bfp.refresh();
+						}
+					}
+				}
+			});
+		}
+	};
 
     /**
      * This runnable is responsible for the validation of the pages.
@@ -226,59 +239,6 @@ public class ModelEditor extends FormEditor
 		
         model = TLCModelFactory.getBy(finput.getFile());
         
-        /*
-         * Install a resource change listener on the file opened which react
-         * on marker changes
-         */
-        // construct the listener
-		modelFileChangeListener = new IResourceChangeListener() {
-			public void resourceChanged(IResourceChangeEvent event) {
-				// get the marker changes
-				IMarkerDelta[] markerChanges = event.findMarkerDeltas(Model.TLC_MODEL_IN_USE_MARKER, false);
-
-				// usually this list has at most one element
-				for (int i = 0; i < markerChanges.length; i++) {
-					if (getFileEditorInput().getFile().equals(markerChanges[i].getResource())) {
-						UIHelper.runUIAsync(
-								/*
-								 * If the model file is changed, refresh the
-								 * changes in the editor if the model is in use,
-								 * activate the third page
-								 */
-								new Runnable() {
-							public void run() {
-								// update the pages
-								for (int i = 0; i < getPageCount(); i++) {
-									/*
-									 * Note that all pages are not necessarily
-									 * instances of BasicFormPage. Some are read
-									 * only editors showing saved versions of
-									 * modules.
-									 */
-									if (pages.get(i) instanceof BasicFormPage) {
-										BasicFormPage page = (BasicFormPage) pages.get(i);
-										((BasicFormPage) page).refresh();
-									}
-								}
-
-								if (model.isRunning()) {
-									showResultPage();
-								}
-								// evtl. add more graphical sugar here,
-								// like changing the model icon,
-								// changing the editor title (part name)
-							}
-						});
-						// It's sufficient to only update the UI once
-						return;
-					}
-				}
-			}
-		};
-
-        // add to the workspace root
-        ResourcesPlugin.getWorkspace().addResourceChangeListener(modelFileChangeListener, IResourceChangeEvent.POST_CHANGE);
- 
         // setContentDescription(path.toString());
         if (model.isSnapshot()) {
         	final String date = sdf.format(model.getSnapshotTimeStamp());
@@ -313,6 +273,8 @@ public class ModelEditor extends FormEditor
 				addPageChangedListener(pageChangedListener);
 			}
 		});
+		
+		model.add(modelStateListener);
 	}
 
 	/**
@@ -342,7 +304,7 @@ public class ModelEditor extends FormEditor
         // TLCUIActivator.getDefault().logDebug("entering ModelEditor#dispose()");
         // remove the listeners
         ResourcesPlugin.getWorkspace().removeResourceChangeListener(workspaceResourceChangeListener);
-        ResourcesPlugin.getWorkspace().removeResourceChangeListener(modelFileChangeListener);
+		model.remove(modelStateListener);
 
         super.dispose();
         
@@ -755,7 +717,7 @@ public class ModelEditor extends FormEditor
 						}
 					}
 				}
-			}, monitor);
+			}, workspace.getRoot(), IWorkspace.AVOID_UPDATE, monitor);
 		} catch (CoreException e) {
 			TLCUIActivator.getDefault().logError(
 					"Error launching the configuration " + model.getName(), e);

org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/launch/TLCModelLaunchDelegate.java
@@ -13,8 +13,6 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.IWorkspaceRunnable;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.Assert;
@@ -30,7 +28,6 @@ import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.jobs.JobChangeAdapter;
-import org.eclipse.core.runtime.jobs.MultiRule;
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
@@ -231,55 +228,46 @@ public class TLCModelLaunchDelegate extends LaunchConfigurationDelegate implemen
                     final boolean recover = config.getAttribute(LAUNCH_RECOVER, LAUNCH_RECOVER_DEFAULT);
                     final IResource[] checkpoints = config.getAdapter(Model.class).getCheckpoints(false);
 
-                    ISchedulingRule deleteRule = ResourceHelper.getDeleteRule(members);
-
                     // delete files
-                    ResourcesPlugin.getWorkspace().run(new IWorkspaceRunnable() {
-
-                        public void run(IProgressMonitor monitor) throws CoreException
+                    boolean checkFiles = recover;
+                    monitor.beginTask("Deleting files", members.length);
+                    // delete the members of the target
+                    // directory
+                    for (int i = 0; i < members.length; i++)
+                    {
+                        if (checkFiles)
                         {
-                            boolean checkFiles = recover;
-                            monitor.beginTask("Deleting files", members.length);
-                            // delete the members of the target
-                            // directory
-                            for (int i = 0; i < members.length; i++)
+                            if (checkpoints.length > 0 && checkpoints[0].equals(members[i]))
                             {
-                                if (checkFiles)
-                                {
-                                    if (checkpoints.length > 0 && checkpoints[0].equals(members[i]))
-                                    {
-                                        // we found the recovery
-                                        // directory and didn't delete
-                                        // it
-                                        checkFiles = false;
-                                        continue;
-                                    }
-                                } else
-                                {
-                                    // delete file
-                                    // either non-recovery mode
-                                    // or the recovery directory already
-                                    // skipped
-                                    try
-                                    {
-                                        if (!members[i].getName().equals(ModelHelper.TE_TRACE_SOURCE))
-                                        {
-                                            members[i].delete(IResource.FORCE, new SubProgressMonitor(monitor, 1));
-                                        }
-                                    } catch (CoreException e)
-                                    {
-                                        // catch the exception if
-                                        // deletion failed, and just
-                                        // ignore this fact
-                                        // FIXME this should be fixed at
-                                        // some later point in time
-                                        TLCActivator.logError("Error deleting a file " + members[i].getLocation(), e);
-                                    }
-                                }
+                                // we found the recovery
+                                // directory and didn't delete
+                                // it
+                                checkFiles = false;
+                                continue;
                             }
-                            monitor.done();
+                        } else
+                        {
+                            // delete file
+                            // either non-recovery mode
+                            // or the recovery directory already
+                            // skipped
+                            try
+                            {
+                                if (!members[i].getName().equals(ModelHelper.TE_TRACE_SOURCE))
+                                {
+                                    members[i].delete(IResource.FORCE, new SubProgressMonitor(monitor, 1));
+                                }
+                            } catch (CoreException e)
+                            {
+                                // catch the exception if
+                                // deletion failed, and just
+                                // ignore this fact
+                                // FIXME this should be fixed at
+                                // some later point in time
+                                TLCActivator.logError("Error deleting a file " + members[i].getLocation(), e);
+                            }
                         }
-                    }, deleteRule, IWorkspace.AVOID_UPDATE, new SubProgressMonitor(monitor, STEP));
+                    }
                 }
             } else
             {
@@ -354,29 +342,16 @@ public class TLCModelLaunchDelegate extends LaunchConfigurationDelegate implemen
                 }
             }
 
-            // get the scheduling rule
-            ISchedulingRule fileRule = MultiRule.combine(ResourceHelper.getModifyRule(files), ResourceHelper
-                    .getCreateRule(files));
-
             // create files
-            ResourcesPlugin.getWorkspace().run(new IWorkspaceRunnable() {
-                public void run(IProgressMonitor monitor) throws CoreException
-                {
-                    for (int i = 0; i < files.length; i++)
-                    {
-                        if (files[i].exists())
-                        {
-                            files[i].setContents(new ByteArrayInputStream("".getBytes()), IResource.DERIVED
-                                    | IResource.FORCE, new SubProgressMonitor(monitor, 1));
-                        } else
-                        {
-                            files[i].create(new ByteArrayInputStream("".getBytes()), IResource.DERIVED
-                                    | IResource.FORCE, new SubProgressMonitor(monitor, 1));
-                        }
-                    }
-                }
-
-            }, fileRule, IWorkspace.AVOID_UPDATE, new SubProgressMonitor(monitor, STEP));
+			for (int i = 0; i < files.length; i++) {
+				if (files[i].exists()) {
+					files[i].setContents(new ByteArrayInputStream("".getBytes()), IResource.DERIVED | IResource.FORCE,
+							new SubProgressMonitor(monitor, 1));
+				} else {
+					files[i].create(new ByteArrayInputStream("".getBytes()), IResource.DERIVED | IResource.FORCE,
+							new SubProgressMonitor(monitor, 1));
+				}
+			}
 
             monitor.worked(STEP);
             monitor.subTask("Creating contents");

org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/model/Model.java
@@ -31,8 +31,10 @@ import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.Vector;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -74,6 +76,7 @@ import org.lamport.tla.toolbox.tool.tlc.TLCActivator;
 import org.lamport.tla.toolbox.tool.tlc.launch.IConfigurationConstants;
 import org.lamport.tla.toolbox.tool.tlc.launch.IModelConfigurationConstants;
 import org.lamport.tla.toolbox.tool.tlc.launch.IModelConfigurationDefaults;
+import org.lamport.tla.toolbox.tool.tlc.model.Model.StateChangeListener.ChangeEvent.State;
 import org.lamport.tla.toolbox.tool.tlc.traceexplorer.SimpleTLCState;
 import org.lamport.tla.toolbox.tool.tlc.util.ModelHelper;
 import org.lamport.tla.toolbox.util.ResourceHelper;
@@ -96,15 +99,11 @@ public class Model implements IModelConfigurationConstants, IAdaptable {
     /**
      * marker on .launch file with boolean attribute modelIsRunning 
      */
-	public static final String TLC_MODEL_IN_USE_MARKER = "org.lamport.tla.toolbox.tlc.modelMarker";
+	private static final String TLC_MODEL_IN_USE_MARKER = "org.lamport.tla.toolbox.tlc.modelMarker";
     /**
      * marker on .launch file, binary semantics
      */
     private static final String TLC_CRASHED_MARKER = "org.lamport.tla.toolbox.tlc.crashedModelMarker";
-    /**
-     * model is being run
-     */
-    private static final String MODEL_IS_RUNNING = "modelIsRunning";
     /**
      * model is locked by a user lock
      */
@@ -136,7 +135,38 @@ public class Model implements IModelConfigurationConstants, IAdaptable {
 	public static Model getByName(final String modelName) {
 		return TLCModelFactory.getByName(modelName);
 	}
+	       
+	/**
+	 * A {@link StateChangeListener} is notified when the running state of the model
+	 * changes. There is no guarantee as to which thread is being used to send the
+	 * notification. A {@link StateChangeListener} has subscribe and unsubscribe
+	 * via {@link Model#add(StateChangeListener)} and {@link Model#remove(StateChangeListener)}.
+	 */
+	public static class StateChangeListener {
 
+		public static class ChangeEvent {
+
+			public enum State {
+				RUNNING, NOT_RUNNING;
+			}
+
+			private final State state;
+
+			private ChangeEvent(State state) {
+				this.state = state;
+			}
+
+			public State getState() {
+				return state;
+			}
+		}
+
+		public void handleChange(ChangeEvent event) {
+			// No-Op
+		}
+	}
+
+	private final Set<StateChangeListener> listeners = new HashSet<StateChangeListener>();
 	private TLCSpec spec;
 	private ILaunchConfiguration launchConfig;
 
@@ -170,6 +200,20 @@ public class Model implements IModelConfigurationConstants, IAdaptable {
 		this.launchConfig = launchConfig;
 	}
 	
+	public boolean add(StateChangeListener stateChangeListener) {
+		return this.listeners.add(stateChangeListener);
+	}
+
+	public boolean remove(StateChangeListener stateChangeListener) {
+		return this.listeners.remove(stateChangeListener);
+	}
+
+	private void notifyListener(final StateChangeListener.ChangeEvent event) {
+		for (StateChangeListener scl : listeners) {
+			scl.handleChange(event);
+		}
+	}
+		       
 	public TLCSpec getSpec() {
 		if (this.spec == null) {
 			final String launchName = this.launchConfig.getName();
@@ -279,7 +323,6 @@ public class Model implements IModelConfigurationConstants, IAdaptable {
 	}
 
 	public boolean isRunning() {
-		final boolean marker = isMarkerSet(TLC_MODEL_IN_USE_MARKER, MODEL_IS_RUNNING);
 		final ILaunchManager launchManager = DebugPlugin.getDefault().getLaunchManager();
 		final ILaunch[] launches = launchManager.getLaunches();
 		for (int i = 0; i < launches.length; i++) {
@@ -287,31 +330,20 @@ public class Model implements IModelConfigurationConstants, IAdaptable {
 			if (launch.getLaunchConfiguration() != null
 					&& launch.getLaunchConfiguration().contentsEqual(this.launchConfig)) {
 				if (!launch.isTerminated()) {
-					if (marker != true) {
-						TLCActivator.logInfo("Model state out-of-sync. Close and reopen spec to sync.");
-					}
 					return true;
 				}
 			}
-		}
-		if (marker != false) {
-			TLCActivator.logInfo("Model state out-of-sync. Close and reopen spec to sync.");
 		}
 		return false;
 	}
 	
 	public void setRunning(boolean isRunning) {
 		if (isRunning) {
-			// Clear the stale/crashed marker. After all, the model is obviously
+			// Clear the crashed marker. After all, the model is obviously
 			// running now.
 			recover();
 		}
-		
-		// There marker is set regardless of the actual isRunning() state
-		// returned by the ILaunchManager above. This is, because the marker
-		// mechanism is used to send "events" to the Toolbox's UI to update the
-		// model editor.
-		setMarker(TLC_MODEL_IN_USE_MARKER, MODEL_IS_RUNNING, isRunning);
+		notifyListener(new StateChangeListener.ChangeEvent(isRunning ? State.RUNNING : State.NOT_RUNNING));
 	}
 
 	public boolean isLocked() {

commit 555162f732d5ceb787fdf12aed9100caec9bdf81

Switch to Model Editor Result Page when model gets run.
Regression caused by 141b40c45289cdbeae871af440595a13a6026592

[Bug][Toolbox]

org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/ui/editor/ModelEditor.java
@@ -113,6 +113,11 @@ public class ModelEditor extends FormEditor
 								bfp.refresh();
 							}
 						}
+						if (event.getState().in(State.RUNNING)) {
+							// Switch to Result Page (put on top) of model editor stack. A user wants to see
+							// the status of a model run she has just started.
+							ModelEditor.this.showResultPage();
+						}
 						if (event.getState().in(State.NOT_RUNNING)) {
 							// Model checking finished, lets open state graph if any.
 							if (event.getModel().hasStateGraphDump()) {
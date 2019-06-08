Jack1106012623/tlacc

commit 6828ae5103559e364da6fe82ae0e8daa636e2d3b

Reflect TLC result in process exit status.
See GitHub issue #277
https://github.com/tlaplus/tlaplus/issues/277

[Feature][TLC][Changelog]

tlatools/src/tlc2/output/EC.java
@@ -8,6 +8,18 @@ package tlc2.output;
  */
 public interface EC
 {
+    /**
+     * Returns an exit status for an error code.
+     */
+    static int exitStatus(int ec) {
+        // TODO Allocate a range of exit status to indicate classes of errors.
+        // For a great example of potential classes see: https://github.com/tlaplus/tlaplus/pull/308#discussion_r285840112
+        return ec == NO_ERROR ? 0 : 1;
+    }
+
+    // This is reserved so that an optional error code can be safely represented in a single int
+    public static final int NO_ERROR = 0;
+
     // Check and CheckImpl
     // check if the TLC option is the same for params
     public static final int CHECK_FAILED_TO_CHECK = 3000;

tlatools/src/tlc2/TLC.java
@@ -243,11 +243,7 @@ public class TLC
         ms.setSpecName(tlc.getSpecName());
 
         // Execute TLC.
-        //
-        // FIXME: process should return a success or failure status,
-        // catching all exceptions thrown by its callees. This makes
-        // process a function we can test.
-        tlc.process();
+        final int errorCode = tlc.process();
 
         // Send logged output by email.
         //
@@ -267,7 +263,7 @@ public class TLC
         }
 
         // Be explicit about tool success.
-        System.exit(0);
+        System.exit(EC.exitStatus(errorCode));
     }
     
 	// false if the environment (JVM, OS, ...) makes model checking impossible.
@@ -876,7 +872,7 @@ public class TLC
 	/**
      * The processing method
      */
-    public void process()
+    public int process()
     {
         // UniqueString.initialize();
         
@@ -897,6 +893,7 @@ public class TLC
     		
     		final RandomGenerator rng = new RandomGenerator();
             // Start checking:
+            final int result;
             if (isSimulate)
             {
                 // random simulation
@@ -913,7 +910,7 @@ public class TLC
 				Simulator simulator = new Simulator(mainFile, configFile, traceFile, deadlock, traceDepth, 
                         traceNum, rng, seed, resolver, TLCGlobals.getNumWorkers());
                 TLCGlobals.simulator = simulator;
-                simulator.simulate();
+                result = simulator.simulate();
             } else
             {
 				if (noSeed) {
@@ -932,32 +929,33 @@ public class TLC
 					TLCGlobals.mainChecker = new ModelChecker(tool, metadir, stateWriter, deadlock, fromChkpt,
 							FPSetFactory.getFPSetInitialized(fpSetConfiguration, metadir, mainFile), startTime);
 					modelCheckerMXWrapper = new ModelCheckerMXWrapper((ModelChecker) TLCGlobals.mainChecker, this);
-					TLCGlobals.mainChecker.modelCheck();
+					result = TLCGlobals.mainChecker.modelCheck();
                 } else
                 {
 					TLCGlobals.mainChecker = new DFIDModelChecker(tool, metadir, stateWriter, deadlock, fromChkpt, startTime);
-					TLCGlobals.mainChecker.modelCheck();
+					result = TLCGlobals.mainChecker.modelCheck();
                 }
             }
+            return result;
         } catch (Throwable e)
         {
             if (e instanceof StackOverflowError)
             {
                 System.gc();
-                MP.printError(EC.SYSTEM_STACK_OVERFLOW, e);
+                return MP.printError(EC.SYSTEM_STACK_OVERFLOW, e);
             } else if (e instanceof OutOfMemoryError)
             {
                 System.gc();
-                MP.printError(EC.SYSTEM_OUT_OF_MEMORY, e);
+                return MP.printError(EC.SYSTEM_OUT_OF_MEMORY, e);
             } else if (e instanceof RuntimeException) 
             {
                 // SZ 29.07.2009 
                 // printing the stack trace of the runtime exceptions
-                MP.printError(EC.GENERAL, e);
+                return MP.printError(EC.GENERAL, e);
                 // e.printStackTrace();
             } else
             {
-                MP.printError(EC.GENERAL, e);
+                return MP.printError(EC.GENERAL, e);
             }
         } finally 
         {

tlatools/src/tlc2/output/ExitStatus.java
@@ -1,0 +1,57 @@
+package tlc2.output;
+
+import static tlc2.output.EC.*;
+
+public class ExitStatus
+{
+
+    public static final int fromErrorCode(int errorCode)
+    {
+        final int exitStatus;
+        switch (errorCode) {
+            
+            case NO_ERROR:
+                exitStatus = 0;
+                break;
+
+            // System errors
+            case SYSTEM_CHECKPOINT_RECOVERY_CORRUPT:
+            case SYSTEM_DISK_IO_ERROR_FOR_FILE:
+            case SYSTEM_DISKGRAPH_ACCESS:
+            case SYSTEM_ERROR_CLEANING_POOL:
+            case SYSTEM_ERROR_READING_POOL:
+            case SYSTEM_ERROR_READING_STATES:
+            case SYSTEM_ERROR_WRITING_POOL:
+            case SYSTEM_ERROR_WRITING_STATES:
+            case SYSTEM_FILE_NULL:
+            case SYSTEM_INDEX_ERROR:
+            case SYSTEM_INTERRUPTED:
+            case SYSTEM_METADIR_CREATION_ERROR:
+            case SYSTEM_METADIR_EXISTS:
+            case SYSTEM_OUT_OF_MEMORY_TOO_MANY_INIT:
+            case SYSTEM_OUT_OF_MEMORY:
+            case SYSTEM_STACK_OVERFLOW:
+            case SYSTEM_STREAM_EMPTY:
+            case SYSTEM_UNABLE_NOT_RENAME_FILE:
+            case SYSTEM_UNABLE_TO_OPEN_FILE:
+                exitStatus = 2;
+                break;
+
+            // Violations
+            case TLC_ACTION_PROPERTY_VIOLATED_BEHAVIOR:
+            case TLC_INVARIANT_VIOLATED_BEHAVIOR:
+            case TLC_INVARIANT_VIOLATED_INITIAL:
+            case TLC_INVARIANT_VIOLATED_LEVEL:
+            case TLC_PROPERTY_VIOLATED_INITIAL:
+            case TLC_TEMPORAL_PROPERTY_VIOLATED:
+                exitStatus = 3;
+                break;
+        
+            default:
+                exitStatus = 255;
+        }
+
+        assert exitStatus >= 0 && exitStatus <= 255;
+        return exitStatus;
+    }
+}

tlatools/src/tlc2/output/MP.java
@@ -1262,9 +1262,9 @@ public class MP
      * Prints the error for a given error code
      * @param errorCode
      */
-    public static void printError(int errorCode)
+    public static int printError(int errorCode)
     {
-        printError(errorCode, EMPTY_PARAMS);
+        return printError(errorCode, EMPTY_PARAMS);
     }
 
     /**
@@ -1272,9 +1272,9 @@ public class MP
      * @param errorCode
      * @param parameter
      */
-    public static void printError(int errorCode, String parameter)
+    public static int printError(int errorCode, String parameter)
     {
-        printError(errorCode, new String[] { parameter });
+        return printError(errorCode, new String[] { parameter });
     }
 
     /**
@@ -1283,13 +1283,14 @@ public class MP
      * @param parameters a list of string parameters to be inserted into the message, by replacing 
      * %i% with the i-th parameter in the array
      */
-    public static void printError(int errorCode, String[] parameters)
+    public static int printError(int errorCode, String[] parameters)
     {
     	recorder.record(errorCode, (Object[]) parameters);
         // write the output
         DebugPrinter.print("entering printError(int, String[]) with errorCode " + errorCode); //$NON-NLS-1$
         ToolIO.out.println(getMessage(ERROR, errorCode, parameters));
         DebugPrinter.print("leaving printError(int, String[])"); //$NON-NLS-1$
+        return errorCode;
     }
 
     /**
@@ -1414,13 +1415,14 @@ public class MP
      * @param errorCode
      * @param cause
      */
-    public static void printError(int errorCode, Throwable cause)
+    public static int printError(int errorCode, Throwable cause)
     {
         if (errorCode == EC.GENERAL) {
             printError(errorCode, "", cause);
         } else {
             printError(errorCode, cause.getMessage(), cause, true);
         }
+        return errorCode;
     }
 
     /**

tlatools/src/tlc2/tool/AbstractChecker.java
@@ -43,6 +43,7 @@ public abstract class AbstractChecker
 	
     protected TLCState predErrState;
     protected TLCState errState;
+    protected int errorCode;
     protected boolean done;
     protected boolean keepCallStack;
     protected final boolean checkDeadlock;
@@ -82,6 +83,7 @@ public abstract class AbstractChecker
         this.errState = null;
         this.predErrState = null;
         this.done = false;
+        this.errorCode = EC.NO_ERROR;
         this.keepCallStack = false;
 
         this.fromChkpt = fromChkpt;
@@ -133,7 +135,7 @@ public abstract class AbstractChecker
      * Set the error state. 
      * <strong>Note:</note> this method must be protected by lock 
      */
-    public boolean setErrState(TLCState curState, TLCState succState, boolean keep)
+    public boolean setErrState(TLCState curState, TLCState succState, boolean keep, int errorCode)
     {
        assert Thread.holdsLock(this) : "Caller thread has to hold monitor!";
        if (!TLCGlobals.continuation && this.done)
@@ -141,6 +143,7 @@ public abstract class AbstractChecker
         IdThread.resetCurrentState();
         this.predErrState = curState;
         this.errState = (succState == null) ? curState : succState;
+        this.errorCode = errorCode;
         this.done = true;
         this.keepCallStack = keep;
         return true;
@@ -363,10 +366,10 @@ public abstract class AbstractChecker
 
     /**
      * Initialize the model checker
-     * @return
+     * @return an error code, or <code>EC.NO_ERROR</code> on success
      * @throws Throwable
      */
-    public abstract boolean doInit(boolean ignoreCancel) throws Throwable;
+    public abstract int doInit(boolean ignoreCancel) throws Throwable;
 
     /**
      * I believe this method is called after the initial states are computed
@@ -375,11 +378,11 @@ public abstract class AbstractChecker
      * Create the partial state space for given starting state up
      * to the given depth or the number of states.
      */
-    public final boolean runTLC(int depth) throws Exception
+    public final int runTLC(int depth) throws Exception
     {
         if (depth < 2)
         {
-            return true;
+            return EC.NO_ERROR;
         }
 
         workers = startWorkers(this, depth);
@@ -419,11 +422,13 @@ public abstract class AbstractChecker
         // SZ Feb 23, 2009: exit if canceled
         // added condition to run in the cycle
         // while (true) {
+        int result = EC.NO_ERROR;
         while (true)
         {
-            if (!this.doPeriodicWork())
+            result = this.doPeriodicWork();
+            if (result != EC.NO_ERROR)
             {
-                return false;
+                return result;
             }
             synchronized (this)
             {
@@ -450,7 +455,7 @@ public abstract class AbstractChecker
         {
             workers[i].join();
         }
-        return true;
+        return EC.NO_ERROR;
     }
     
 	public final void setAllValues(int idx, IValue val) {
@@ -485,10 +490,10 @@ public abstract class AbstractChecker
      * Check liveness: check liveness properties on the partial state graph.
      * Checkpoint: checkpoint three data structures: the state set, the
      *             state queue, and the state trace.
-     * @return
+     * @return an error code, or <code>EC.NO_ERROR</code> on success
      * @throws Exception
      */
-    public abstract boolean doPeriodicWork() throws Exception;
+    public abstract int doPeriodicWork() throws Exception;
 
     /**
      * Method called from the main worker loop
@@ -500,9 +505,15 @@ public abstract class AbstractChecker
 
     /**
      * Main method of the model checker
+     * @return an error code, or <code>EC.NO_ERROR</code> on success
      * @throws Exception
      */
-    public abstract void modelCheck() throws Exception;
+    final public int modelCheck() throws Exception {
+        final int result = modelCheckImpl();
+        return (result != EC.NO_ERROR) ? result : errorCode;
+    }
+
+    protected abstract int modelCheckImpl() throws Exception;
 
 	public int getProgress() {
 		return -1;

tlatools/src/tlc2/tool/CheckImpl.java
@@ -6,6 +6,7 @@ package tlc2.tool;
 import java.io.IOException;
 
 import tlc2.TLCGlobals;
+import tlc2.output.EC;
 import tlc2.output.StatePrinter;
 import tlc2.tool.fp.FPSet;
 import tlc2.tool.fp.FPSetConfiguration;
@@ -69,10 +70,11 @@ public abstract class CheckImpl extends ModelChecker {
       this.doInit(false);
     }
     ToolIO.out.println("Creating a partial state space of depth " +
-		       this.depth + " ... ");
-    if (!this.runTLC(this.depth)) {
+           this.depth + " ... ");
+    final int result = this.runTLC(this.depth);
+    if (result != EC.NO_ERROR) {
       ToolIO.out.println("\nExit: failed to create the partial state space.");
-      System.exit(1);
+      System.exit(EC.exitStatus(result));
     }
     ToolIO.out.println("completed.");
     this.lastTraceTime = System.currentTimeMillis();
@@ -93,8 +95,9 @@ public abstract class CheckImpl extends ModelChecker {
     int depth1= this.trace.getLevel(st.uid) + depth;
     this.theStateQueue = new DiskStateQueue(this.metadir);
     this.theStateQueue.enqueue(st);
-    if (!this.runTLC(depth1)) {
-      System.exit(1);
+    final int result = this.runTLC(depth1);
+    if (result != EC.NO_ERROR) {
+      System.exit(EC.exitStatus(result));
     }
   }
   

commit 3730f2970e250783d7236ae16c987bcb2535fa3e

Toolbox shows TLC as crashed on violation of safety or liveness violation.
Regression caused by commit 6828ae5103559e364da6fe82ae0e8daa636e2d3b

[Bug][Toolbox]

org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/job/TLCProcessJob.java
@@ -29,6 +29,7 @@ import org.lamport.tla.toolbox.tool.tlc.output.internal.BroadcastStreamListener;
 import org.lamport.tla.toolbox.util.ResourceHelper;
 
 import tlc2.TLC;
+import tlc2.output.EC;
 import tlc2.tool.fp.FPSetFactory;
 import tlc2.tool.fp.NoopFPSet;
 import tlc2.tool.fp.OffHeapDiskFPSet;
@@ -437,6 +438,10 @@ public class TLCProcessJob extends TLCJob
     			shouldNotHappen.printStackTrace();
     		}
     	}
-    	return -1;
+    	return 255;
     }
+
+	public boolean hasCrashed() {
+		return EC.ExitStatus.exitStatusToCrash(getExitValue());
+	}
 }

org.lamport.tla.toolbox.tool.tlc/src/org/lamport/tla/toolbox/tool/tlc/launch/TLCModelLaunchDelegate.java
@@ -1061,7 +1061,7 @@ public class TLCModelLaunchDelegate extends LaunchConfigurationDelegate implemen
             	Assert.isNotNull(event.getResult());
             	TLCProcessJob tlcJob = (TLCProcessJob) event.getJob();
 
-				if (!Status.CANCEL_STATUS.equals(event.getJob().getResult()) && tlcJob.getExitValue() > 0) {
+				if (!Status.CANCEL_STATUS.equals(event.getJob().getResult()) && tlcJob.hasCrashed()) {
 					// if TLC crashed with exit value > 0 and the user did *not*
 					// click cancel, mark the job as crashed.
 					model.setStale();

tlatools/src/tlc2/output/EC.java
@@ -1,5 +1,8 @@
 package tlc2.output;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Interface containing the error code constants 
@@ -395,5 +398,13 @@ public interface EC
 				return 255;
 			}
 	    }
+
+		private static final Set<Integer> knownExitValues = Stream.of(SUCCESS, FAILURE_LIVENESS_EVAL, FAILURE_SPEC_EVAL,
+				FAILURE_SAFETY_EVAL, VIOLATION_SAFETY, VIOLATION_LIVENESS, VIOLATION_DEADLOCK, VIOLATION_ASSUMPTION,
+				VIOLATION_ASSERT, ERROR_CONFIG_PARSE, ERROR_SPEC_PARSE).collect(Collectors.toSet());
+		
+		public static boolean exitStatusToCrash(final int exitStatus) {
+			return !knownExitValues.contains(exitStatus);
+		}
     }
 }
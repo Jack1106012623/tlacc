Jack1106012623/tlacc

commit b46fa38052fdc98a4279557fd22f37a529b705f7

Error-Trace shows bogus values - #372
- Part of the underlying Tree implementation is relying on the item's hashcode, so our purposing of the hash calculations to make it easy to filter all TLCVariables based on a single proto-variable instance was resulting in the data for one instance of TLCVariable being shown for all of the same name/is-explorer couplet value. Have discontinued overriding hashcode for our filtering purposes.

#372
[Bug][Regression][Toolbox]

org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/ui/view/TLCErrorView.java
@@ -109,6 +109,7 @@ public class TLCErrorView extends ViewPart
 				+ "\u2022 Double-click on a line to go to corresponding action in spec \u2014 "
 				+ "or while holding down " + (Platform.getOS().equals(Platform.OS_MACOSX) ? "\u2318" : "CTRL")
 				+ " to go to the original PlusCal code, if present.\n"
+				+ "\u2022 Click on a variable while holding down ALT to hide the variable from view.\n"
 				+ "\u2022 Right-click on a location row for a context menu.";
 
     /**
@@ -466,10 +467,10 @@ public class TLCErrorView extends ViewPart
         				
         				if (selection instanceof TLCVariable) {
         					if (filterErrorTraceAction.isChecked()) {
-        						currentErrorTraceFilterSet.add((TLCVariable)selection);
+        						addVariableFamilyToFiltering((TLCVariable)selection);
         					} else {
         						currentErrorTraceFilterSet.clear();
-        						currentErrorTraceFilterSet.add((TLCVariable)selection);
+        						addVariableFamilyToFiltering((TLCVariable)selection);
 								filterErrorTraceAction.setChecked(true);
         					}
         					
@@ -813,6 +814,16 @@ public class TLCErrorView extends ViewPart
 	
 	TreeViewer getViewer() {
 		return errorTraceTreeViewer.getTreeViewer();
+	}
+	
+	private void addVariableFamilyToFiltering(final TLCVariable protoVariable) {
+		for (final TLCState state : unfilteredInput.getStates(TLCError.Length.ALL)) {
+			for (final TLCVariable variable : state.getVariablesAsList()) {
+				if (protoVariable.representsTheSameAs(variable)) {
+					currentErrorTraceFilterSet.add(variable);
+				}
+			}
+		}
 	}
 	
 	private void performVariableViewPopulation(final EnumSet<FilterType> filters) {

org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/output/data/TLCVariable.java
@@ -110,33 +110,11 @@ public class TLCVariable
 	public final boolean isChanged() {
 		return value.isAdded() || value.isDeleted() || value.isChanged();
 	}
-
+	
 	/**
-	 * This only included name and whether this is a trace explorer variable in the calculation.
+	 * This compares against name and the value of {@code isTraceExplorerVar}
 	 */
-	@Override
-	public int hashCode() {
-		return Objects.hash(isTraceExplorerVar, name);
-	}
-
-	/**
-	 * This only included name and whether this is a trace explorer variable in the determination.
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		
-		if (obj == null) {
-			return false;
-		}
-		
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		
-		final TLCVariable other = (TLCVariable) obj;
+	public boolean representsTheSameAs(final TLCVariable other) {
 		return (isTraceExplorerVar == other.isTraceExplorerVar) && Objects.equals(name, other.name);
 	}
 }

commit a5f0c5080ebfbafe993157c7af4e590de394c809

Error-trace variable filtering only works for one state - #386
- Fixing regression introduced with b46fa38052fdc98a4279557fd22f37a529b705f7
- Providing RCPTT test

#386
[Regression][Toolbox]

org.lamport.tla.toolbox.product.uitest/Functional/TLA.Functional.Error.Trace.test
@@ -6,7 +6,7 @@ Element-Version: 3.0
 External-Reference: 
 Id: _BbLbgO0TEemu8qbF8JrHog
 Runtime-Version: 2.4.0.201902010011
-Save-Time: 10/14/19, 11:30 AM
+Save-Time: 11/4/19, 9:49 AM
 Testcase-Type: ecl
 
 ------=_.content-0a7243a0-75d3-3d5f-9791-539de0e5b7ac
@@ -73,6 +73,20 @@ try -command {
 				concat $stateOneP1Val | equals $stateTwoP1Val | verify-false
 			}
 		}
+	}
+	
+	
+	// The following is testing for regressions like the one behind Issue 386, which is, sadly, a regression due to 372.
+	get-view $TLA-VIEW-TLC-ERRORS | get-section $TLA-MO-ED-TLCERRORS-SECTION-ET
+			| get-button "Click to select variables and expressions to omit from the trace display; ALT-click on an individual item below to omit it immediately."
+			| click
+	with [ get-window "Error Trace Filter" ] {
+		get-table | get-item -path " b" | check
+		get-button "OK" | click
+	}
+	with [get-view $TLA-VIEW-TLC-ERRORS | get-section $TLA-MO-ED-TLCERRORS-SECTION-ET | get-tree] {
+		get-object | invoke getItem 0 | invoke getItem 0 | invoke getText 0 | str | equals " c" | verify-true
+		get-object | invoke getItem 1 | invoke getItem 0 | invoke getText 0 | str | equals " b" | verify-false
 	}
 } -finally {
 	DeleteSpecNew

org.lamport.tla.toolbox.tool.tlc.ui/src/org/lamport/tla/toolbox/tool/tlc/ui/view/TLCErrorView.java
@@ -838,8 +838,18 @@ public class TLCErrorView extends ViewPart
 				//		so we do this clone and filter of the input data.
 				for (final TLCState filteredState : filtered.getStates(TLCError.Length.ALL)) {
 					final List<TLCVariable> variables = filteredState.getVariablesAsList();
+					final ArrayList<TLCVariable> removals = new ArrayList<>();
 					
-					variables.removeAll(currentErrorTraceFilterSet);
+					for (final TLCVariable variable : variables) {
+						for (final TLCVariable filterVariable : currentErrorTraceFilterSet) {
+							if (filterVariable.representsTheSameAs(variable)) {
+								removals.add(variable);
+								break;
+							}
+						}
+					}
+					
+					variables.removeAll(removals);
 				}
 			}
 
@@ -923,7 +933,7 @@ public class TLCErrorView extends ViewPart
 		private static final String SELECTED_TOOL_TIP_TEXT = "Click to display all variables and expressions.";
 		
 		FilterErrorTrace() {
-			super("Filter the displayed variables and expressions..", AS_CHECK_BOX);
+			super("Filter the displayed variables and expressions", AS_CHECK_BOX);
 			
 			final ImageDescriptor id = TLCUIActivator.getImageDescriptor("icons/elcl16/trace_filter.png");
 			setImageDescriptor(id);
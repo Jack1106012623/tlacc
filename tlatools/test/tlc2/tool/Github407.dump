Jack1106012623/tlacc

commit ee1617b05fbcdf55684217e239b3a6c1ac5967e3

Visualize state space in dot notation.
Add "dot" command line sub-parameter that causes TLC to write the dump file in dot notation.

[Feature][TLC]

tlatools/src/tlc2/util/StateWriter.java
@@ -9,12 +9,11 @@ import util.FileUtil;
 /**
  * State writer 
  * @author Simon Zambrovski
- * @version $Id$
  */
-public class StateWriter
+public class StateWriter implements IStateWriter
 {
-    private PrintWriter writer;
-    private int stateNum;
+    protected final PrintWriter writer;
+    protected int stateNum;
 
     public StateWriter(String fname) throws IOException
     {
@@ -23,6 +22,9 @@ public class StateWriter
         this.stateNum = 1;
     }
 
+    /* (non-Javadoc)
+	 * @see tlc2.util.IStateWriter#writeState(tlc2.tool.TLCState)
+	 */
     public synchronized void writeState(TLCState state)
     {
         this.writer.println("State " + this.stateNum + ":");
@@ -30,7 +32,17 @@ public class StateWriter
         this.stateNum++;
     }
 
-    public final void close()
+    /* (non-Javadoc)
+	 * @see tlc2.util.IStateWriter#writeState(tlc2.tool.TLCState, tlc2.tool.TLCState, boolean)
+	 */
+    public synchronized void writeState(final TLCState state, final TLCState successor, final boolean successorStateIsNew)
+    {
+    	if (successorStateIsNew) {
+    		this.writeState(state);
+    	}
+    }
+
+    public void close()
     {
         this.writer.close();
     }

tlatools/customBuild.xml
@@ -203,6 +203,10 @@
 				<pathelement path="${class.dir}" />
 			</classpath>
 		</javac>
+		<!-- Include auxilliary files when compiling classes. -->
+		<copy todir="${test.class.dir}">
+		   <fileset dir="${test.dir}" includes="**/*.dot"/>
+		</copy>		
 		<!-- copy class.dir to path with whitespace -->
 		<!-- this is required by some tests to make sense -->
 		<!-- even throw a non digit in -->

tlatools/src/tlc2/TLC.java
@@ -68,6 +68,17 @@ public class TLC
     private String mainFile;
     private String configFile;
     private String dumpFile;
+    /**
+	 * If true, the dumpFile will be written in dot format to be processed by
+	 * GraphViz.
+	 * <p>
+	 * Contrary to plain -dump, -dot will also write out transitions from state
+	 * s to s' if s' is already known. Thus, the resulting graph shows all
+	 * successors of s instead of just s's unexplored ones.
+	 * <p>
+	 * Off/False by default.
+	 */
+    private boolean asDot;
     private String fromChkpt;
 
     private int fpIndex;
@@ -105,6 +116,7 @@ public class TLC
         mainFile = null;
         configFile = null;
         dumpFile = null;
+        asDot = false;
         fromChkpt = null;
         resolver = null;
 
@@ -146,7 +158,8 @@ public class TLC
      *    Defaults to 1
      *  o -dfid num: use depth-first iterative deepening with initial depth num
      *  o -cleanup: clean up the states directory
-     *  o -dump file: dump all the states into file
+     *  o -dump [dot] file: dump all the states into file. If "dot" as sub-parameter
+     *                      is given, the output will be in dot notation.
      *  o -difftrace: when printing trace, show only
      *                the differences between successive states
      *    Defaults to printing full state descriptions if not specified
@@ -298,14 +311,24 @@ public class TLC
                 }
             } else if (args[index].equals("-dump"))
             {
+            	String suffix = ".dump";
+            	
                 index++;
+                if (index < args.length && args[index].equals("dot"))
+                {
+                    asDot = true;
+                    suffix = ".dot";
+                    index++;
+                }
                 if (index < args.length)
                 {
                     dumpFile = args[index];
+					// Require a $suffix file extension unless already given. It
+					// is not clear why this is enforced.
                     int len = dumpFile.length();
-                    if (!(dumpFile.startsWith(".dump", len - 5)))
+                    if (!(dumpFile.startsWith(suffix, len - suffix.length())))
                     {
-                        dumpFile = dumpFile + ".dump";
+                        dumpFile = dumpFile + suffix;
                     }
                     index++;
                 } else
@@ -760,11 +783,11 @@ public class TLC
                 AbstractChecker mc = null;
                 if (TLCGlobals.DFIDMax == -1)
                 {
-                    mc = new ModelChecker(mainFile, configFile, dumpFile, deadlock, fromChkpt, resolver, specObj, fpSetConfiguration);
+                    mc = new ModelChecker(mainFile, configFile, dumpFile, asDot, deadlock, fromChkpt, resolver, specObj, fpSetConfiguration);
                     modelCheckerMXWrapper = new ModelCheckerMXWrapper((ModelChecker) mc);
                 } else
                 {
-                    mc = new DFIDModelChecker(mainFile, configFile, dumpFile, deadlock, fromChkpt, true, resolver, specObj);
+                    mc = new DFIDModelChecker(mainFile, configFile, dumpFile, asDot, deadlock, fromChkpt, true, resolver, specObj);
                 }
                 TLCGlobals.mainChecker = mc;
 // The following statement moved to Spec.processSpec by LL on 10 March 2011               

tlatools/src/tlc2/tool/AbstractChecker.java
@@ -13,7 +13,10 @@ import tlc2.tool.liveness.ILiveCheck;
 import tlc2.tool.liveness.LiveCheck;
 import tlc2.tool.liveness.Liveness;
 import tlc2.tool.liveness.NoOpLiveCheck;
+import tlc2.util.DotStateWriter;
+import tlc2.util.IStateWriter;
 import tlc2.util.IdThread;
+import tlc2.util.NoopStateWriter;
 import tlc2.util.ObjLongTable;
 import tlc2.util.StateWriter;
 import tlc2.util.statistics.BucketStatistics;
@@ -27,7 +30,6 @@ import util.FilenameToStream;
 /**
  * The abstract checker
  * @author Simon Zambrovski
- * @version $Id$
  */
 public abstract class AbstractChecker implements Cancelable
 {
@@ -58,7 +60,7 @@ public abstract class AbstractChecker implements Cancelable
     public Action[] impliedActions;
     public Action[] impliedInits;
     public Action[] actions;
-    protected StateWriter allStateWriter;
+    protected final IStateWriter allStateWriter;
     protected boolean cancellationFlag;
     private IdThread[] workers;
 	protected final ILiveCheck liveCheck;
@@ -82,7 +84,7 @@ public abstract class AbstractChecker implements Cancelable
      * @param resolver
      * @param spec - pre-built specification object (e.G. from calling SANY from the tool previously)
      */
-    public AbstractChecker(String specFile, String configFile, String dumpFile, boolean deadlock, String fromChkpt,
+    public AbstractChecker(String specFile, String configFile, String dumpFile, final boolean asDot, boolean deadlock, String fromChkpt,
             boolean preprocess, FilenameToStream resolver, SpecObj spec) throws EvalException, IOException
     {
         this.cancellationFlag = false;
@@ -112,7 +114,13 @@ public abstract class AbstractChecker implements Cancelable
         // Initialize dumpFile:
         if (dumpFile != null)
         {
-            this.allStateWriter = new StateWriter(dumpFile);
+        	if (asDot) {
+        		this.allStateWriter = new DotStateWriter(dumpFile);
+        	} else {
+        		this.allStateWriter = new StateWriter(dumpFile);
+        	}
+        } else {
+        	 this.allStateWriter = new NoopStateWriter();
         }
 
         this.impliedInits = this.tool.getImpliedInits(); // implied-inits to be checked

tlatools/src/tlc2/tool/CheckImpl.java
@@ -34,7 +34,7 @@ public abstract class CheckImpl extends ModelChecker {
 		   int depth, String fromChkpt, final FPSetConfiguration fpSetConfig)
   throws IOException {
     // SZ Feb 20, 2009: patched due to changes to ModelCheker
-    super(specFile, configFile, null, deadlock, fromChkpt, null, null, fpSetConfig); // no name resolver and no specobj
+    super(specFile, configFile, null, false, deadlock, fromChkpt, null, null, fpSetConfig); // no name resolver and no specobj
     this.depth = depth;
     this.curState = null;
     this.coverSet = FPSetFactory.getFPSet();

tlatools/src/tlc2/tool/DFIDModelChecker.java
@@ -42,11 +42,11 @@ public class DFIDModelChecker extends AbstractChecker
      * Constructor for running DFID   
      * @param resolver 
      */
-    public DFIDModelChecker(String specFile, String configFile, String dumpFile, boolean deadlock, String fromChkpt,
+    public DFIDModelChecker(String specFile, String configFile, String dumpFile, boolean asDot, boolean deadlock, String fromChkpt,
             boolean preprocess, FilenameToStream resolver, SpecObj specObj) throws EvalException, IOException
     {
         // call the abstract constructor
-        super(specFile, configFile, dumpFile, deadlock, fromChkpt, preprocess, resolver, specObj);
+        super(specFile, configFile, dumpFile, asDot, deadlock, fromChkpt, preprocess, resolver, specObj);
 
         this.theInitStates = null;
         this.theInitFPs = null;
@@ -267,10 +267,7 @@ public class DFIDModelChecker extends AbstractChecker
                         this.theInitFPs[idx++] = fp;
 
                         // Write out the state if asked
-                        if (this.allStateWriter != null)
-                        {
-                            this.allStateWriter.writeState(curState);
-                        }
+                        this.allStateWriter.writeState(curState);
 
                         // build behavior graph for liveness checking
                         if (this.checkLiveness)
@@ -390,10 +387,7 @@ public class DFIDModelChecker extends AbstractChecker
                         allSuccNonLeaf = allSuccNonLeaf && !FPIntSet.isLeaf(status);
 
                         // Write out the state when new and asked:
-                        if (status == FPIntSet.NEW && this.allStateWriter != null)
-                        {
-                            this.allStateWriter.writeState(succState);
-                        }
+                        this.allStateWriter.writeState(curState, succState, status == FPIntSet.NEW);
 
                         // Remember succState if it has not been completed at this level:
                         if (!FPIntSet.isCompleted(status))
@@ -683,8 +677,7 @@ public class DFIDModelChecker extends AbstractChecker
         this.theFPSet.close();
         if (this.checkLiveness)
             liveCheck.close();
-        if (this.allStateWriter != null)
-            this.allStateWriter.close();
+        this.allStateWriter.close();
         // SZ Feb 23, 2009:
         // FileUtil.deleteDir(new File(this.metadir), success);
         FileUtil.deleteDir(this.metadir, success);

tlatools/src/tlc2/tool/ModelChecker.java
@@ -72,11 +72,11 @@ public class ModelChecker extends AbstractChecker
      * @param specObj external SpecObj added to enable to work on existing specification 
      * Modified on 6 Apr 2010 by Yuan Yu to add fpMemSize parameter.
      */
-    public ModelChecker(String specFile, String configFile, String dumpFile, boolean deadlock, String fromChkpt,
+    public ModelChecker(String specFile, String configFile, String dumpFile, final boolean asDot, boolean deadlock, String fromChkpt,
             FilenameToStream resolver, SpecObj specObj, final FPSetConfiguration fpSetConfig) throws EvalException, IOException
     {
         // call the abstract constructor
-        super(specFile, configFile, dumpFile, deadlock, fromChkpt, true, resolver, specObj);
+        super(specFile, configFile, dumpFile, asDot, deadlock, fromChkpt, true, resolver, specObj);
 
         // SZ Feb 20, 2009: this is a selected alternative
         this.theStateQueue = new DiskStateQueue(this.metadir);
@@ -425,13 +425,10 @@ public class ModelChecker extends AbstractChecker
                     {
 						long fp = succState.fingerPrint();
 						seen = this.theFPSet.put(fp);
+                        // Write out succState when needed:
+                        this.allStateWriter.writeState(curState, succState, !seen);
                         if (!seen)
                         {
-							// Write out succState when needed:
-                            if (this.allStateWriter != null)
-                            {
-								this.allStateWriter.writeState(succState);
-							}
 							// Write succState to trace only if it satisfies the
 							// model constraints. Do not enqueue it yet, but wait
                             // for implied actions and invariants to be checked.
@@ -798,12 +795,11 @@ public class ModelChecker extends AbstractChecker
         this.trace.close();
         if (this.checkLiveness)
             liveCheck.close();
-        if (this.allStateWriter != null)
-            this.allStateWriter.close();
-        	if (!VETO_CLEANUP) {
-        		FileUtil.deleteDir(this.metadir, success);
-        	}
-		}
+        this.allStateWriter.close();
+    	if (!VETO_CLEANUP) {
+    		FileUtil.deleteDir(this.metadir, success);
+    	}
+	}
 
     public final void printSummary(boolean success, final long startTime) throws IOException
     {
@@ -1175,9 +1171,7 @@ public class ModelChecker extends AbstractChecker
 					long fp = curState.fingerPrint();
 					seen = theFPSet.put(fp);
 					if (!seen) {
-						if (allStateWriter != null) {
-							allStateWriter.writeState(curState);
-						}
+						allStateWriter.writeState(curState);
 						curState.uid = trace.writeState(fp);
 						theStateQueue.enqueue(curState);
 

tlatools/src/tlc2/util/DotStateWriter.java
@@ -1,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Microsoft Research. All rights reserved. 
+ *
+ * The MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributors:
+ *   Markus Alexander Kuppe - initial API and implementation
+ ******************************************************************************/
+
+package tlc2.util;
+
+import java.io.IOException;
+
+import tlc2.tool.TLCState;
+import tlc2.util.StateWriter;
+
+/**
+ * Writes the given state in dot notation.
+ * 
+ * @see https://en.wikipedia.org/wiki/DOT_(graph_description_language)
+ */
+public class DotStateWriter extends StateWriter {
+
+	public DotStateWriter(final String fname) throws IOException {
+		super(fname);
+		this.writer.append("strict digraph DiskGraph {\n"); // strict removes redundant edges
+		this.writer.append("rankdir=LR;\n"); // Left to right rather than top to bottom
+	}
+
+	/* (non-Javadoc)
+	 * @see tlc2.util.StateWriter#writeState(tlc2.tool.TLCState)
+	 */
+	public synchronized void writeState(final TLCState state) {
+		// Marker the state as an initial state by using a filled style.
+		this.writer.append(Long.toString(state.fingerPrint()));
+		this.writer.append(" [style = filled]");
+		this.writer.append(" [label=\"");
+		this.writer.append(states2dot(state));
+		this.writer.append("\"]");
+		this.writer.append("\n");
+	}
+	
+	/* (non-Javadoc)
+	 * @see tlc2.util.StateWriter#writeState(tlc2.tool.TLCState, tlc2.tool.TLCState, boolean)
+	 */
+	public synchronized void writeState(TLCState state, TLCState successor, boolean successorStateIsNew) {
+		final String successorsFP = Long.toString(successor.fingerPrint());
+
+		// Write the transition
+		this.writer.append(Long.toString(state.fingerPrint()));
+		this.writer.append(" -> ");
+		this.writer.append(successorsFP);
+		this.writer.append(";\n");
+
+		// If the successor is new, we have to print the label of the state.
+		// Otherwise we would print its fingerprint.
+		if (successorStateIsNew) {
+			// Write the successor's label
+			this.writer.append(successorsFP);
+			this.writer.append(" [label=\"");
+			this.writer.append(states2dot(successor));
+			this.writer.append("\"]");
+			this.writer.append(";\n");
+		}
+	}
+	
+	private static String states2dot(final TLCState state) {
+		// Replace "\" with "\\" and """ with "\"".	
+		return state.toString().replace("\\", "\\\\").replace("\"", "\\\"").trim();
+	}
+
+	/* (non-Javadoc)
+	 * @see tlc2.util.IStateWriter#close()
+	 */
+	public void close() {
+		this.writer.append("}");
+		super.close();
+	}
+}

commit b388eb2ca49b656add2d1e742a8bc4be2ce3ce1c

States missing from TLC state space dump not in dot format.
Regression introduced with git commit
ee1617b05fbcdf55684217e239b3a6c1ac5967e3

https://github.com/tlaplus/tlaplus/issues/407

[Bug][TLC]

tlatools/src/tlc2/util/StateWriter.java
@@ -62,14 +62,14 @@ public class StateWriter implements IStateWriter
     public synchronized void writeState(final TLCState state, final TLCState successor, final boolean successorStateIsNew)
     {
     	if (successorStateIsNew) {
-    		this.writeState(state);
+    		this.writeState(successor);
     	}
     }
 
     public synchronized void writeState(final TLCState state, final TLCState successor, final boolean successorStateIsNew, Action action)
     {
     	if (successorStateIsNew) {
-    		this.writeState(state);
+    		this.writeState(successor);
     	}
     }
     
@@ -78,7 +78,7 @@ public class StateWriter implements IStateWriter
      */
     public void writeState(TLCState state, TLCState successor, final boolean successorStateIsNew, final Visualization visualization) {
     	if (successorStateIsNew) {
-    		this.writeState(state);
+    		this.writeState(successor);
     	}
     }
     

tlatools/test-model/Github407.cfg
@@ -1,0 +1,4 @@
+INIT
+Init
+NEXT
+Next

tlatools/test-model/Github407.tla
@@ -1,0 +1,9 @@
+---- MODULE Github407 ----
+Data == {"d1", "d2"}
+
+VARIABLES state
+
+Init == state = {}
+
+Next == \E e \in Data: state' = state \union {e}
+=============================================================================

tlatools/test/tlc2/tool/Github407.dump
@@ -1,0 +1,12 @@
+State 1:
+state = {}
+
+State 2:
+state = {"d1"}
+
+State 3:
+state = {"d2"}
+
+State 4:
+state = {"d1", "d2"}
+

tlatools/test/tlc2/tool/Github407Test.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Microsoft Research. All rights reserved. 
+ * Copyright (c) 2019 Microsoft Research. All rights reserved. 
  *
  * The MIT License (MIT)
  * 
@@ -25,12 +25,11 @@
  ******************************************************************************/
 package tlc2.tool;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
@@ -41,43 +40,34 @@ import tlc2.output.EC;
 import tlc2.output.EC.ExitStatus;
 import tlc2.tool.liveness.ModelCheckerTestCase;
 
-public class DumpAsDotTest extends ModelCheckerTestCase {
+public class Github407Test extends ModelCheckerTestCase {
 
-	public DumpAsDotTest() {
-		super("MCa", "CodePlexBug08", new String[] { "-dump", "dot,colorize,actionlabels",
-				System.getProperty("java.io.tmpdir") + File.separator + "DumpAsDotTest" }, ExitStatus.VIOLATION_LIVENESS);
+	public Github407Test() {
+		super("Github407",
+				new String[] { "-dump", System.getProperty("java.io.tmpdir") + File.separator + "Github407" },
+				ExitStatus.SUCCESS);
 	}
 
 	@Test
-	public void testSpec() throws IOException {
-		// ModelChecker has finished and generated the expected amount of states
+	public void testSpec() throws FileNotFoundException, IOException {
 		assertTrue(recorder.recorded(EC.TLC_FINISHED));
-		assertFalse(recorder.recorded(EC.GENERAL));
-		assertTrue(recorder.recordedWithStringValues(EC.TLC_STATS, "18", "11", "0"));
+		assertTrue(recorder.recordedWithStringValues(EC.TLC_STATS, "9", "4", "0"));
+		assertTrue(recorder.recordedWithStringValue(EC.TLC_SEARCH_DEPTH, "3"));
 		
-		// -dump appends the ".dump" extension to the file name
-		final File dumpFile = new File(System.getProperty("java.io.tmpdir") + File.separator + "DumpAsDotTest.dot");
+		final File dumpFile = new File(System.getProperty("java.io.tmpdir") + File.separator + "Github407.dump");
 		assertTrue(dumpFile.exists());
 		
 		// If the file exist, simply compare it to a correct and manually checked version.
-		final InputStream master = getClass().getResourceAsStream("DumpAsDotTest.dot");
-		assertTrue(Arrays.equals(getBytes(master), getBytes(new FileInputStream(dumpFile))));
+		final InputStream master = getClass().getResourceAsStream("Github407.dump");
+		assertTrue(Arrays.equals(DumpAsDotTest.getBytes(master), DumpAsDotTest.getBytes(new FileInputStream(dumpFile))));
 
 		assertZeroUncovered();
+		
 	}
-	
-	// http://stackoverflow.com/a/17861016
-	public static byte[] getBytes(InputStream is) throws IOException {
-		final ByteArrayOutputStream os = new ByteArrayOutputStream();
-		try {
-			byte[] buffer = new byte[0xFFFF];
-			for (int len; (len = is.read(buffer)) != -1;) {
-				os.write(buffer, 0, len);
-			}
-			os.flush();
-			return os.toByteArray();
-		} finally {
-			os.close();
-		}
+
+	@Override
+	protected boolean doDump() {
+		// Create the non-dot dump explicitly through constructor above.
+		return false;
 	}
 }
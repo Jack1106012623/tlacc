Jack1106012623/tlacc

commit 0e204669b04029aca222ae60421c9db3934f8575

Do not warp EvalException with TLCRuntimeException when module overwrite fails to evaluate.
Avoids printing the Java exception as part of the error message.

[Refactor][TLC]

tlatools/src/tlc2/tool/ModelChecker.java
@@ -1138,7 +1138,7 @@ public class ModelChecker extends AbstractChecker
 						}
 					}
 				}
-			} catch (InvariantViolatedException | Assert.TLCRuntimeException e) {
+			} catch (InvariantViolatedException | Assert.TLCRuntimeException | EvalException e) {
 				// IVE gets thrown above when an Invariant is violated. TLCRuntimeException gets
 				// thrown when Tool fails to evaluate a statement because of e.g. too large sets
 				// or type errors such as in DoInitFunctorInvariantMinimalErrorStackTest test.

tlatools/src/tlc2/tool/distributed/TLCServer.java
@@ -452,10 +452,10 @@ public class TLCServer extends UnicastRemoteObject implements TLCServerRMI,
 				// would require synchronization among all (distributed)
 				// workers. In distributed mode, it is of limited use anyway. 
 				if (e instanceof EvalException
-						&& ((EvalException) e).getErrorCode() == EC.TLC_MODULE_VALUE_JAVA_METHOD_OVERRIDE
-						&& (((EvalException) e).getMessage().contains("tlc2.module.TLC.TLCSet")
-								|| ((EvalException) e).getMessage().contains("tlc2.module.TLC.TLCGet"))
-						|| (e instanceof TLCRuntimeException && ((TLCRuntimeException) e).errorCode == 2154)) {
+						&& ((EvalException) e).getErrorCode() == EC.TLC_MODULE_TLCGET_UNDEFINED
+						&& (((EvalException) e).getMessage().contains("TLCSet")
+								|| ((EvalException) e).getMessage().contains("TLCGet"))
+						|| (e instanceof TLCRuntimeException && ((TLCRuntimeException) e).errorCode == EC.TLC_MODULE_VALUE_JAVA_METHOD_OVERRIDE)) {
 					MP.printError(EC.TLC_FEATURE_UNSUPPORTED,
 							"TLCSet & TLCGet operators not supported by distributed TLC.");
 				} else {

tlatools/src/tlc2/tool/impl/SpecProcessor.java
@@ -68,6 +68,7 @@ import tlc2.tool.Action;
 import tlc2.tool.BuiltInOPs;
 import tlc2.tool.Defns;
 import tlc2.tool.EvalControl;
+import tlc2.tool.EvalException;
 import tlc2.tool.Specs;
 import tlc2.tool.TLCState;
 import tlc2.tool.TLCStateMut;
@@ -221,7 +222,7 @@ public class SpecProcessor implements ValueConstants, ToolGlobals {
               IValue defVal = spec.eval(opDef.getBody(), Context.Empty, TLCState.Empty, CostModel.DO_NOT_RECORD);
               defVal.deepNormalize();
               consts[i].setToolObject(toolId, defVal);
-            } catch (Assert.TLCRuntimeException e) {
+            } catch (Assert.TLCRuntimeException | EvalException e) {
               Assert.fail(EC.TLC_CONFIG_SUBSTITUTION_NON_CONSTANT,
                   new String[] { consts[i].getName().toString(), opDef.getName().toString() });
             }

tlatools/src/tlc2/value/impl/MethodValue.java
@@ -126,6 +126,9 @@ public class MethodValue extends OpValue implements Applicable {
               throw new EvalException(EC.TLC_MODULE_VALUE_JAVA_METHOD_OVERRIDE, new String[]{this.md.toString(), targetException.getMessage()});
           } else if (e instanceof NullPointerException) {
               throw new EvalException(EC.TLC_MODULE_VALUE_JAVA_METHOD_OVERRIDE, new String[]{this.md.toString(), e.getMessage()});
+          } else if (e instanceof EvalException) {
+        	  // Do not wrap an EvalException below.
+        	  throw (EvalException) e;
           } else
           {
               Assert.fail(EC.TLC_MODULE_VALUE_JAVA_METHOD_OVERRIDE, new String[]{this.md.toString(), e.getMessage()});

tlatools/test/tlc2/tool/doinitfunctor/DoInitFunctorInvariantMinimalErrorStackTest.java
@@ -25,7 +25,6 @@
  ******************************************************************************/
 package tlc2.tool.doinitfunctor;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -43,12 +42,9 @@ public class DoInitFunctorInvariantMinimalErrorStackTest extends ModelCheckerTes
 	public void testSpec() {
 		assertTrue(recorder.recorded(EC.TLC_FINISHED));
 		assertTrue(recorder.recordedWithStringValues(EC.TLC_STATS, "1", "1", "1"));
-		assertFalse(recorder.recorded(EC.GENERAL));
 
-		assertTrue(recorder.recordedWithStringValues(EC.TLC_MODULE_VALUE_JAVA_METHOD_OVERRIDE,
-				"public static tlc2.value.IBoolValue tlc2.module.Naturals.GEQ(tlc2.value.impl.Value,tlc2.value.impl.Value)",
-				"The first argument of > should be an integer, but instead it is:\n" + 
-				"<<1, 1>>"));
+		assertTrue(recorder.recordedWithStringValues(EC.TLC_MODULE_ARGUMENT_ERROR_AN,
+				"first", ">", "integer", "<<1, 1>>"));
 
 		String errorStack = "0. Line 8, column 3 to line 9, column 13 in DoInitFunctorMinimalErrorStack\n"
 				+ "1. Line 8, column 6 to line 8, column 25 in DoInitFunctorMinimalErrorStack\n"

commit 4f9fdb234be9bbd5b6104111569d32afbc615408

TLC shows no error trace on EvalExceptions thrown by module overrides.
Expressions that cause EvalExceptions in module overrides are silly
expressions like `x < TRUE`, but also a violated TLC!Assert, bogus
TLCGet/TLCSet, ....

TLC module overrides used to wrap nested EvalExceptions (the root cause)
in a (more technical) TLCRuntimeException, which was changed in commit
6569d03ca1be31ae1cf3cef0a23b005cbc6ecc3 to not wrap EvalExceptions. The
goal was to tell users the root cause. This, however, introduced a
regression because the global exception handling was not updated to
print an error-trace on EvalException.  Instead, the error on stdout
omitted the trace and only showed the stack trace pointing to the spec
location of the TLA+ expression.

Fixes Github issue #461
https://github.com/tlaplus/tlaplus/issues/461

Regression introduced with commit
0e204669b04029aca222ae60421c9db3934f8575
https://github.com/tlaplus/tlaplus/commit/0e204669b04029aca222ae60421c9db3934f8575

[Regression][Bug][TLC][Changelog]

tlatools/org.lamport.tlatools/src/tlc2/tool/EvalException.java
@@ -21,28 +21,38 @@ public class EvalException extends RuntimeException
 
 
     private final int errorCode;
+    private final String[] parameters;
 
 	public EvalException(int errorCode, String[] parameters)
     {
         super(MP.getMessage(errorCode, parameters));
 		this.errorCode = errorCode;
+		this.parameters = parameters;
     }
 
     public EvalException(int errorCode, String parameter)
     {
-        super(MP.getMessage(errorCode, parameter));
-		this.errorCode = errorCode;
+    	this(errorCode, new String[] {parameter});
     }
 
     public EvalException(int errorCode)
     {
         super(MP.getMessage(errorCode));
-		this.errorCode = errorCode;        
+		this.errorCode = errorCode;
+		this.parameters = null;
     }
 
     public int getErrorCode() {
     	return errorCode;
     }
+
+	public String[] getParameters() {
+		return parameters;
+	}
+
+	public boolean hasParameters() {
+		return parameters != null;
+	}
     
     // SZ Jul 14, 2009: refactored and deprecated, all usage changed to standard constructor 
     // public EvalException(int type, String message)

tlatools/org.lamport.tlatools/src/tlc2/tool/ModelChecker.java
@@ -282,6 +282,10 @@ public class ModelChecker extends AbstractChecker
                 } catch (FingerprintException e)
                 {
                     result = MP.printError(EC.TLC_FINGERPRINT_EXCEPTION, new String[]{e.getTrace(), e.getRootCause().getMessage()});
+                } catch (EvalException e) {
+                	// Do not replace the actual error code, such as assert violation, with TLC_NESTED_EXPRESSION.
+	                MP.printError(EC.TLC_NESTED_EXPRESSION, cTool.toString());
+	                result = e.getErrorCode();
                 } catch (Throwable e)
                 {
                     // Assert.printStack(e);
@@ -635,6 +639,9 @@ public class ModelChecker extends AbstractChecker
 			} else if (e instanceof AssertionError)
 			{
 				ec = EC.TLC_BUG;
+			} else if (e instanceof EvalException)
+			{
+				ec = ((EvalException) e).getErrorCode();
 			} else
 			{
 				ec = EC.GENERAL;
@@ -644,7 +651,15 @@ public class ModelChecker extends AbstractChecker
 		    {
 				if (!(ec == EC.GENERAL && e.getMessage() == null))
 		        {
-					MP.printError(ec, e);
+					if (e instanceof EvalException && ((EvalException) e).hasParameters()) {
+						// An EvalException pretty-prints itself in its constructor, i.e. converts the
+						// parameters into the human readable string. However, MP.print* will
+						// pretty-print it a second time, which is why we pass the original parameters
+						// instead of the EvalException itself.  Exception handling in TLC is a mess!
+						MP.printError(ec, ((EvalException) e).getParameters(), e);
+					} else {
+						MP.printError(ec, e);
+					}
 		        }
 				this.trace.printTrace(curState, succState);
 				this.theStateQueue.finishAll();

tlatools/org.lamport.tlatools/src/tlc2/tool/Worker.java
@@ -98,7 +98,7 @@ public final class Worker extends IdThread implements IWorker, INextStateFunctor
 				final long preNext = this.statesGenerated;
 				try {
 					this.tool.getNextStates(this, curState);
-				} catch (TLCRuntimeException e) {
+				} catch (TLCRuntimeException | EvalException e) {
 					// The next-state relation couldn't be evaluated.
 					this.tlc.doNextFailed(curState, null, e);
 				}

tlatools/org.lamport.tlatools/test-model/Github461.cfg
@@ -1,0 +1,4 @@
+INIT
+Init
+NEXT
+Next

tlatools/org.lamport.tlatools/test-model/Github461.tla
@@ -1,0 +1,12 @@
+----------------------------- MODULE Github461 -----------------------------
+EXTENDS Integers, TLC
+
+VARIABLES x
+
+Init == x = 0
+
+Next == 
+    /\ Assert(x < 4, "Failure of assertion at line 8, column 4.")
+    /\ x' = x + 1
+
+====

tlatools/org.lamport.tlatools/test/tlc2/tool/Github461Test.java
@@ -1,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Microsoft Research. All rights reserved. 
+ *
+ * The MIT License (MIT)
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy 
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software. 
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Contributors:
+ *   Markus Alexander Kuppe - initial API and implementation
+ ******************************************************************************/
+package tlc2.tool;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import tlc2.output.EC;
+import tlc2.tool.liveness.ModelCheckerTestCase;
+
+public class Github461Test extends ModelCheckerTestCase {
+
+	public Github461Test() {
+		super("Github461", EC.ExitStatus.VIOLATION_ASSERT);
+	}
+
+	@Test
+	public void testSpec() throws FileNotFoundException, IOException {
+		// Assert evaluation error.
+		assertTrue(recorder.recordedWithStringValue(EC.TLC_VALUE_ASSERT_FAILED,
+				"\"Failure of assertion at line 8, column 4.\""));
+
+		// Assert an error trace.
+		assertTrue(recorder.recorded(EC.TLC_STATE_PRINT2));
+		
+		// Assert the correct trace.
+		final List<String> expectedTrace = new ArrayList<String>(4);
+		expectedTrace.add("x = 0");
+		expectedTrace.add("x = 1");
+		expectedTrace.add("x = 2");
+		expectedTrace.add("x = 3");
+		expectedTrace.add("x = 4");
+		assertTraceWith(recorder.getRecords(EC.TLC_STATE_PRINT2), expectedTrace);
+
+		// Assert the underlying error message with stack trace.
+		assertTrue(recorder.recordedWithStringValues(EC.TLC_NESTED_EXPRESSION,
+				"0. Line 9, column 5 to line 10, column 17 in Github461\n" + 
+				"1. Line 9, column 8 to line 9, column 65 in Github461\n" + 
+				"\n"));
+		
+		assertZeroUncovered();
+	}
+}
82b025900ccaaa34c4f0fdc65b9a65a066b8b5bd
Jack1106012623 <1106012623@qq.com>
2023-10-10 23:28:52 +0800

    for sync

---
+++ tlatools/org.lamport.tlatools/src/tlc2/cc/ActionMap.java
@@ -0,0 +1,26 @@
+package tlc2.cc;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+import tlc2.tool.Action;
+
+public class ActionMap {
+	// action name -> actions with different args
+	public HashMap<String, List<Action>> name2list;
+	
+    // fill name2list
+	public void fill() {
+		Action[] actions = CC.actions;
+		for (Action action : actions) {
+			String name = action.getNameOfDefault();
+			if(name2list.containsKey(name)) {
+				name2list.put(name, new ArrayList<>());
+			}
+			name2list.get(name).add(action);
+		}
+	}
+	
+}
